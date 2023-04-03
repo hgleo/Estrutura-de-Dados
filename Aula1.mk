d38a542da51a7d90e7c67063e740e6a95a046c48
hgleo <hleomelo1@gmail.com>
2023-04-03 14:20:16 -0300

    COMMIT

---
+++ Aula1.mk
@@ -0,0 +1,16 @@
+# Avaliação:
+	2 provas
+	Trabalhos de implementação(Classroom, GoogleDrive)
+
+# Conteúdo:
+	- Listas Recusivas
+	- Árvores Binárias
+	- Árvores Binárias de Busca(ABB)
+	- Árvore AVL
+	- Árvore RN e B
+	- Heap
+	- Hashing (Tabela de dispersão)
+	- Backtracking
+	- Tipo abstrato de dados
+	- Grafos
+	- Estruturas Genéricas

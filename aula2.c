8c74330f56b36a8d7e4c367665d44aa63d514d21
hgleo <hleomelo1@gmail.com>
2023-04-04 19:42:21 -0300

    commit

--- aula2.c
+++ aula2.c
@@ -1,3 +1,7 @@
+// Somar todos os elementos da lista ~ok  
+// Contar a recorrencia de números   ~ok
+// Remover um elemento               ~ok
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -38,6 +42,38 @@ int somatorioLista(lista *l){
 
 }
 
+lista *inserirFinal(lista *l,int x){
+  if(l ==NULL){
+    lista *no = (lista*)malloc(sizeof(lista));
+    no->info = x;
+    no->prox = NULL;
+    return no;
+
+  }
+
+  else
+    l->prox = inserirFinal(l->prox,x);
+
+  return l;
+}
+
+int contaRecorrencia(lista *l, int x){
+
+  int recorrencia = 0;
+
+  if(l == NULL){
+    return recorrencia;
+  }
+  else{
+    if(l->info == x){
+      recorrencia += 1;
+    }
+    return recorrencia + contaRecorrencia(l->prox,x);
+  }
+
+}
+
+/*
 void inserirFinal(lista **l, int x){
 
   if (*l == NULL) {
@@ -49,24 +85,58 @@ void inserirFinal(lista **l, int x){
   }
   
 }
+*/
+
+lista *removerElemento(lista *l, int x) {
+
+  if (l == NULL) {
+    return l;
+  }
+
+  if (l->info == x) {
+    lista *tmp = l;
+    l = l->prox;
+    free(tmp);
+    return l;
+  }
+
+  lista *anterior = l;
+  lista *atual = l->prox;
+
+  while (atual != NULL && atual->info != x) {
+    anterior = atual;
+    atual = atual->prox;
+  }
+
+  if (atual != NULL) {
+    anterior->prox = atual->prox;
+    free(atual);
+  }
+
+  return l;
+}
+
 
 int main(){
 
   lista *l = NULL;
 
-  inserirFinal(&l, 24);
-  inserirFinal(&l, 10);
-  inserirFinal(&l, 4);
-  inserirFinal(&l, 2);
+  l = inserirFinal(l, 24);
+  l = inserirFinal(l, 10);
+  l = inserirFinal(l, 4);
+  l = inserirFinal(l, 2);
 
   imprimeLista(l);
 
+  printf("\n 0 se não existe 1 caso exista: %d ", existe(l, 2));
+  printf("\n 0 se não existe 1 caso exista: %d ", existe(l, 3));
+  printf("\n Qts vezes o numero %d se repete: %d ", 2 ,contaRecorrencia(l, 2));
+
+  printf("\nSomatório: %d ", somatorioLista(l));
+
+  l = removerElemento(l, 2);
   printf("\n");
-
-  printf("%d\n", existe(l, 2));
-  printf("%d\n", existe(l, 3));
-
-  printf("%d\n", somatorioLista(l));
+  imprimeLista(l);
 
   return 0;
 }

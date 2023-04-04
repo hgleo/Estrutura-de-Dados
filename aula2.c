09ec70f45658f689306cee875d875db858a6bd22
hgleo <hleomelo1@gmail.com>
2023-04-04 14:13:39 -0300

    commit

--- aula2.c
+++ aula2.c
@@ -1,69 +1,72 @@
-// ### Exemplo 1: Verificar se um elemento x existe na lista. (Retorna 1 caso exista, 0 caso não exista)
-// Enunciado: Somatorio da lista.
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct lista{
+typedef struct lista {
+
   int info;
   struct lista *prox;
-}lista;
+} lista;
 
-int existe(lista *l,int x){
+int existe(lista *l, int x){
 
-  if(l == NULL){
+  if (l == NULL) {
     return 0;
-  }
-
-  else if(l->info == x){
+  } else if (l->info == x) {
     return 1;
-  }
-
-  else{
-    return existe(l->prox,x);
+  } else {
+    return existe(l->prox, x);
   }
 
 }
+
 void imprimeLista(lista *l){
-  
-  if(l != NULL){
+
+  if (l != NULL) {
     printf("%d ", l->info);
     imprimeLista(l->prox);
   }
 
-
-int somatorioLista(lista *l){
-  if(l == NULL){
-    return 0;
-  }
-  else{
-    return l->info + somatorioLista(l->prox);
-  }
 }
 
-void inserirFinal(lista **l,int x){
-  if(*l == NULL){
+int somatorioLista(lista *l){
+
+  if (l == NULL) {
+    return 0;
+  } else {
+    return l->info + somatorioLista(l->prox);
+  }
+
+}
+
+void inserirFinal(lista **l, int x){
+
+  if (*l == NULL) {
     *l = (lista *)malloc(sizeof(lista));
     (*l)->info = x;
     (*l)->prox = NULL;
+  } else {
+    inserirFinal(&(*l)->prox, x);
   }
-  else{
-    inserirFinal(&(*l)->prox,x);
-  }
+  
 }
 
-}
-int main(lista *l){
+int main(){
 
-  l = inserirFinal(&l,24);
-  l = inserirFinal(&l,10);
-  l = inserirFinal(&l,4);
-  l = inserirFinal(&l,2);
+  lista *l = NULL;
+
+  inserirFinal(&l, 24);
+  inserirFinal(&l, 10);
+  inserirFinal(&l, 4);
+  inserirFinal(&l, 2);
 
   imprimeLista(l);
 
-  existe(l,2);
-  existe(l,3);
+  printf("\n");
 
-  l = somatorioLista(l);
+  printf("%d\n", existe(l, 2));
+  printf("%d\n", existe(l, 3));
+
+  printf("%d\n", somatorioLista(l));
 
   return 0;
 }
